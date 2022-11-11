a79cfe0f39ca6acbcad1823c74d5922fbdd37b4d
Rexe0 <tombring12@gmail.com>
2022-11-12 09:19:47 +1000

    Initial Commit

---
+++ src/main/java/me/rexe0/uhcchampions/UHCChampions.java
@@ -0,0 +1,17 @@
+package me.rexe0.uhcchampions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UHCChampions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
