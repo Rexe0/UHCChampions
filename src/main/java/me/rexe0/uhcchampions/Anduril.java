efdac4b93eba9069cd8b4a8588d111a12de45a8f
Rexe0 <tombring12@gmail.com>
2022-11-12 13:55:39 +1000

    Fixed bug with anduril check

--- src/main/java/me/rexe0/uhcchampions/Anduril.java
+++ src/main/java/me/rexe0/uhcchampions/Anduril.java
@@ -1,13 +1,14 @@
 package me.rexe0.uhcchampions;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 public class Anduril {
     public static void andurilCheck(Player player) {
-        if (player.getItemInHand() == null) return;
+        if (player.getItemInHand() == null || player.getItemInHand().getType() == Material.AIR) return;
         if (!player.getItemInHand().getItemMeta().hasDisplayName()) return;
         if (!player.getItemInHand().getItemMeta().getDisplayName().equals(ChatColor.GREEN+"Anduril")) return;
         player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 20, 0));
