fb6b86c10f5ddc5989b27f3289f54f20083a0b20
Rexe0 <tombring12@gmail.com>
2022-11-12 13:49:32 +1000

    Added Anduril

--- src/main/java/me/rexe0/uhcchampions/UHCChampions.java
+++ src/main/java/me/rexe0/uhcchampions/UHCChampions.java
@@ -1,6 +1,9 @@
 package me.rexe0.uhcchampions;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public final class UHCChampions extends JavaPlugin {
 
@@ -9,6 +12,14 @@ public final class UHCChampions extends JavaPlugin {
         // Plugin startup logic
         getServer().getPluginManager().registerEvents(new PlayerHead(), this);
         getServer().addRecipe(PlayerHead.goldenHeadCraft());
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                for (Player player : Bukkit.getOnlinePlayers())
+                    Anduril.andurilCheck(player);
+            }
+        }.runTaskTimer(this, 20, 10);
     }
 
     @Override
