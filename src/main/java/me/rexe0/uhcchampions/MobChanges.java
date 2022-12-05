76961c87c84aa414955ceab47ceed5090e5835af
Rexe0 <tombring12@gmail.com>
2022-12-05 15:03:49 +1000

    Added minimum drops for some mobs. Wither skeletons now have a change to replace blazes replacing pigmen

--- src/main/java/me/rexe0/uhcchampions/MobChanges.java
+++ src/main/java/me/rexe0/uhcchampions/MobChanges.java
@@ -2,12 +2,11 @@ package me.rexe0.uhcchampions;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.PigZombie;
-import org.bukkit.entity.Sheep;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerShearEntityEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -19,11 +18,15 @@ public class MobChanges implements Listener {
     public void onSpawn(CreatureSpawnEvent e) {
         if (e.getSpawnReason() != CreatureSpawnEvent.SpawnReason.NATURAL) return;
         if (!(e.getEntity() instanceof PigZombie)) return;
-        if (rand.nextInt(3) != 0) return;
-        Location location = e.getEntity().getLocation();
-        location.getWorld().spawnEntity(location, EntityType.BLAZE);
-
-        e.setCancelled(true);
+        if (rand.nextInt(3) == 0) {
+            Location location = e.getEntity().getLocation();
+            e.setCancelled(true);
+            if (rand.nextInt(3) == 0) {
+                ((Skeleton)location.getWorld().spawnEntity(location, EntityType.SKELETON)).setSkeletonType(Skeleton.SkeletonType.WITHER);
+                return;
+            }
+            location.getWorld().spawnEntity(location, EntityType.BLAZE);
+        }
 
     }
     @EventHandler
@@ -31,5 +34,26 @@ public class MobChanges implements Listener {
         if (!(e.getEntity() instanceof Sheep)) return;
         if (rand.nextInt(3) != 0) return;
         e.getEntity().getWorld().dropItemNaturally(e.getEntity().getLocation(), new ItemStack(Material.STRING));
+    }
+    @EventHandler
+    public void onDeath(EntityDeathEvent e) {
+        LivingEntity entity = e.getEntity();
+        if (entity instanceof Spider) {
+            entity.getWorld().dropItemNaturally(entity.getLocation(), new ItemStack(Material.STRING));
+            return;
+        }
+        if (entity instanceof Chicken) {
+            entity.getWorld().dropItemNaturally(entity.getLocation(), new ItemStack(Material.FEATHER));
+            return;
+        }
+        if (entity instanceof Skeleton) {
+            for (ItemStack item : e.getDrops())
+                if (item.getType() == Material.BOW && rand.nextInt(5) == 0) item.setDurability((short)0);
+            return;
+        }
+        if (entity instanceof Ghast) {
+            entity.getWorld().dropItemNaturally(entity.getLocation(), new ItemStack(Material.NETHER_WARTS));
+            return;
+        }
     }
 }
