27fc3e85d9149c00c1f542818f13c6e7cba8b6b3
Rexe0 <tombring12@gmail.com>
2024-08-21 19:58:29 +1000

    Reworked chalice of grace

--- main/src/main/java/me/rexe0/uhcchampions/items/ChaliceOfGrace.java
+++ main/src/main/java/me/rexe0/uhcchampions/items/ChaliceOfGrace.java
@@ -2,69 +2,63 @@ package me.rexe0.uhcchampions.items;
 
 import me.rexe0.uhcchampions.UHCChampions;
 import me.rexe0.uhcchampions.config.ConfigLoader;
+import me.rexe0.uhcchampions.util.PotionEffectType;
 import me.rexe0.uhcchampions.util.Sound;
 import me.rexe0.uhcchampions.util.VersionUtils;
 import org.bukkit.Material;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.scheduler.BukkitRunnable;
-
-import java.util.HashSet;
-import java.util.UUID;
+import org.bukkit.potion.PotionEffect;
 
 public class ChaliceOfGrace implements Listener {
-    private final HashSet<UUID> chaliceEffect = new HashSet<>();
     private static final String id = "chalice-of-grace";
+
     @EventHandler
-    public void onClick(PlayerInteractEvent e) {
-        if (e.getAction() != Action.RIGHT_CLICK_AIR && e.getAction() != Action.RIGHT_CLICK_BLOCK) return;
-        ItemStack item = e.getItem();
+    public void onPlace(PlayerInteractEvent e) {
+        if (!e.getAction().toString().contains("RIGHT")) return;
         ConfigLoader loader = UHCChampions.getConfigLoader();
-
-        if (!UHCChampions.isItem(item, loader.getItemName(id))) return;
-        Player player = e.getPlayer();
-
-        chaliceEffect.add(player.getUniqueId());
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                chaliceEffect.remove(player.getUniqueId());
-            }
-        }.runTaskLater(UHCChampions.getInstance(), loader.getItemInteger(id, "duration"));
-
-        player.playSound(player.getLocation(), Sound.IRON_GOLEM_DEATH.getSound(), 1, 0);
-        int amount = item.getAmount();
-        amount--;
-        if (amount <= 0) {
-            VersionUtils.getVersionUtils().setItemInMainhand(player, new ItemStack(Material.AIR));
-        } else {
-            item.setAmount(amount);
-            VersionUtils.getVersionUtils().setItemInMainhand(player, item);
-        }
+        if (!UHCChampions.isItem(e.getItem(), loader.getItemName(id))) return;
         e.setCancelled(true);
     }
-
     @EventHandler
-    public void onHit(EntityDamageByEntityEvent e) {
-        if (!(e.getEntity() instanceof Player) || !(e.getDamager() instanceof LivingEntity)) return;
+    public void onHit(EntityDamageEvent e) {
+        if (!(e.getEntity() instanceof Player)) return;
         Player player = (Player) e.getEntity();
-        if (!chaliceEffect.contains(player.getUniqueId()) || e.isCancelled()) return;
+        if (e.isCancelled()) return;
 
-        double damage = e.getFinalDamage();
-        e.setDamage(0.1);
+        if (player.getHealth()-e.getFinalDamage() > 1) return;
+        ConfigLoader loader = UHCChampions.getConfigLoader();
 
-        double multiplier = UHCChampions.getConfigLoader().getItemDouble(id, "cap");
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                UHCChampions.dealTrueDamage(player, Math.min(VersionUtils.getVersionUtils().getMaxHealth(player)*multiplier, damage));
+        ItemStack[] contents = player.getInventory().getContents();
+        for (int i = 0; i < contents.length; i++) {
+            ItemStack item = contents[i];
+            if (!UHCChampions.isItem(item, loader.getItemName(id))) continue;
+
+            if (trigger(player, item)) {
+                item.setType(Material.AIR);
+                contents[i] = item;
+                player.getInventory().setContents(contents);
             }
-        }.runTaskLater(UHCChampions.getInstance(), 1);
+            e.setCancelled(true);
+            return;
+        }
+    }
+
+    private boolean trigger(Player player, ItemStack item) {
+        ConfigLoader loader = UHCChampions.getConfigLoader();
+        player.setHealth(5);
+        player.addPotionEffect(new PotionEffect(VersionUtils.getVersionUtils().getPotionEffectType(PotionEffectType.RESISTANCE), loader.getItemInteger(id, "duration"), 4), true);
+
+        player.getWorld().playSound(player.getLocation(), Sound.IRON_GOLEM_DEATH.getSound(), 1, 0);
+        int amount = item.getAmount();
+        amount--;
+        if (amount <= 0)
+            return true;
+        item.setAmount(amount - 1);
+        return false;
     }
 }
