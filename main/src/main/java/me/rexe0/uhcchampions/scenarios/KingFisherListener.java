443da9758163f358d03604565829c0b41a87abee
Rexe0 <tombring12@gmail.com>
2024-08-23 23:59:44 +1000

    Further increased king fisher chance

--- main/src/main/java/me/rexe0/uhcchampions/scenarios/KingFisherListener.java
+++ main/src/main/java/me/rexe0/uhcchampions/scenarios/KingFisherListener.java
@@ -29,7 +29,7 @@ public class KingFisherListener extends ScenarioListener {
     }
 
     // Percent chance of fishing up custom item.
-    private final int chance = 4;
+    private final int chance = 5;
 
     @EventHandler
     public void onFish(PlayerFishEvent e) {
